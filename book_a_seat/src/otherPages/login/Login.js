f072d8be3d97e22ce8054e3db9b42ae8c41e1bac
Abhinay5465 <abhinaygarg15@gmail.com>
2024-08-08 12:17:03 +0530

    m

--- book_a_seat/src/otherPages/login/Login.js
+++ book_a_seat/src/otherPages/login/Login.js
@@ -101,6 +101,7 @@ const Login = () => {
   const userRef = useRef();
   const errRef = useRef();
 
+  const [user, setUser] = useState('');
   const [pwd, setPwd] = useState('');
   const [errMsg, setErrMsg] = useState('');
   const [success, setSuccess] = useState(false);
